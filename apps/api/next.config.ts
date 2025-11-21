c044d2fec92fc313375ac9ce124739fa68e42a5f
nafisaziziriza <nafisaziziriza@gmail.com>
2025-11-21 21:33:48 +1000

    Revert "fix: add workaround plugin"
    This reverts commit ed18b783a03dfa5a6b2637eaa9c107060b7ed6db.

--- apps/api/next.config.ts
+++ apps/api/next.config.ts
@@ -1,18 +1,9 @@
-import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 import { config, withAnalyzer } from "@repo/next-config";
 import { withLogging, withSentry } from "@repo/observability/next-config";
 import type { NextConfig } from "next";
 import { env } from "@/env";
 
-let nextConfig: NextConfig = {
-  ...withLogging(config),
-  webpack: (webpackConfig, { isServer }) => {
-    if (isServer) {
-      webpackConfig.plugins = [...webpackConfig.plugins, new PrismaPlugin()];
-    }
-    return webpackConfig;
-  },
-};
+let nextConfig: NextConfig = withLogging(config);
 
 if (env.VERCEL) {
   nextConfig = withSentry(nextConfig);
--- apps/app/prisma-plugin.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "@prisma/nextjs-monorepo-workaround-plugin" {
-  import type { Compiler } from "webpack";
-
-  export class PrismaPlugin {
-    constructor();
-    apply(compiler: Compiler): void;
-  }
-}
